5537729e4b994800b188b6a045851e8517defe99
Brad <PoYi.Lee@yuanta.com>
2019-01-10 16:45:14 +0800

    change db setting to clearDB

--- MySQL/select.js
+++ MySQL/select.js
@@ -1,10 +1,10 @@
 let mysql = require('mysql');
 
 let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '123456',
-    database: 'mydb'
+    host: 'us-cdbr-iron-east-01.cleardb.net',
+    user: 'be2486c3e96d49',
+    password: 'f80c050d',
+    database: 'heroku_83260ccd1f595f9'
 });
 
 function query() {
