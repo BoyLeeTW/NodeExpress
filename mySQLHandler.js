b9f899b1d62eef28ecb9f8c2c925b852e75b77f7
Brad <PoYi.Lee@yuanta.com>
2019-01-11 11:57:32 +0800

    modify credential for clearDB

--- mySQLHandler.js
+++ mySQLHandler.js
@@ -1,14 +1,14 @@
 let mysql = require('mysql');
 
 let connection = mysql.createConnection({
-    // host: 'us-cdbr-iron-east-01.cleardb.net',
-    // user: 'be2486c3e96d49',
-    // password: 'f80c050d',
-    // database: 'heroku_83260ccd1f595f9'
-    host: 'localhost',
-    user: 'root',
-    password: '123456',
-    database: 'mydb'
+    host: 'us-cdbr-iron-east-01.cleardb.net',
+    user: 'be2486c3e96d49',
+    password: 'f80c050d',
+    database: 'heroku_83260ccd1f595f9'
+    // host: 'localhost',
+    // user: 'root',
+    // password: '123456',
+    // database: 'mydb'
 
 });
 
@@ -16,6 +16,7 @@ function connect(completion) {
     connection.connect(function(err) {
         if (err) {
             completion('Connect failed' + err);
+            return
         }
         completion('Connected!');
     });
